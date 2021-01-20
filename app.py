1b92a3ec799b4ae9b8648c52aed485df93b297f1
vmware_ubuntu <vmware@ubuntu.com>
2021-01-20 11:27:08 -0800

    uwsgi version

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def do_magic():
 
 def application(env, start_response):
     start_response('200 OK',[('Content-type','text\html')])
-    return  [do_magic()]
+    return  [do_magic().encode()]
 
 
 
