7c0e64d32eddb5b91007e86bd14b306994957fe7
vmware_ubuntu <vmware@ubuntu.com>
2021-01-20 11:08:42 -0800

    uwsgi version

--- app.py
+++ app.py
@@ -3,12 +3,17 @@
 import datetime
 import os
 
-def pr():
+def do_magic():
     now =datetime.datetime.now()
-    print ("Hello {0}".format(now))
+    return "Hello {0}".format(now) 
+
+def application(env, start_response):
+    start_response('200 OK',[('Content-type','text\html')])
+    return  [do_magic()]
+
 
 
 if __name__== "__main__":
     if "REQUEST_URI" in os.environ:
         print("Content-type: text-html\n\n")
-    pr()
+    print(do_magic())
